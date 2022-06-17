d12b0fb3280695c3f5b76e5cc5ac4bfb6ccd1a6a
nirajmohanrana <nirajmohanrana@gmail.com.com>
2022-06-17 11:09:27 +0530

    day 2 17-6

---
+++ Day 2/Recursion/2^m/2raisetom.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int tworaisetom(int);
+
+int main() {
+    int m, result;
+
+    cout<<"Enter the value of m: ";
+    cin>>m;
+
+    result = tworaisetom(m);
+    cout << "2 raise to " << m << " = " << result;
+    return 0;
+}
+
+int tworaisetom(int m) {
+    if (m > 1) {
+        return 2 * tworaisetom(m - 1);
+    } else {
+        return 1;
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # 90-days-DSA-Challenge
 
-Challenge to complete DSA topics in hopefully 90 Days
+## Challenge to complete DSA topics in hopefully 90 Days
 
-Day 1: Time Complexity
+**Day 1**: Time Complexity `8.6.22|☑️`
+
+**Day 2**: Space Complexity `9.6.22|☑️`
+
+**Day 3**: Recursion `17.6.22|`
