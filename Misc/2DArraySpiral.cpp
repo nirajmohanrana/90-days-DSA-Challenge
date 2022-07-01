7337126b2e1c08f1ba284894ddb28b812568c96f
nirajmohanrana <nirajmohanrana@gmail.com.com>
2022-07-01 10:23:14 +0530

    Misc: Spiral Print

--- Misc/2DArraySpiral.cpp
+++ Misc/2DArraySpiral.cpp
@@ -12,10 +12,35 @@ int main() {
         }
     }
 
-    for (int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cout<<arr[i][j]<<" ";
+    int row_start = 0, row_end=r-1,
+        col_start = 0, col_end=c-1;
+
+    while(row_start <= row_end && col_start <= col_end) {
+        // row start
+        for(int i = col_start; i<=col_end; i++) {
+            cout<<arr[row_start][i]<<" ";
         }
-        cout<<endl;
+
+        row_start++;
+
+        // col end
+        for(int i=row_start; i<=row_end; i++){
+            cout<<arr[i][col_end]<<" ";
+        }
+
+        col_end--;
+
+        // row end
+        for(int i = col_end; i>=col_start; i--){
+            cout<<arr[row_end][i]<<" ";
+        }
+
+        row_end--;
+
+        // col start
+        for(int i = row_end; i>=row_start; i--){
+            cout<<arr[i][col_start]<<" ";
+        }
+        col_start++;
     }
 }
